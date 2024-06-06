129b7c4da3217f94771691dd3d3e7ba06b8751e8
Angelica Farias Oliveira <angelica.oliveira@inmetrics.com.br>
2024-06-06 13:26:48 -0300

    Projeto AWS Lambda

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lambda em Go na AWS
+
+Este repositório cria um código e em seguida faz um deploy na AWS utilizando o lambda, uma ferramenta serveless para criar máquinas.
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayProxyRequest) (
+	events.APIGatewayProxyResponse, error) {
+
+	 
+
+	 fmt.Println("Hello world")
+
+	 response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	 }
+
+	 return response, nil
+
+}
